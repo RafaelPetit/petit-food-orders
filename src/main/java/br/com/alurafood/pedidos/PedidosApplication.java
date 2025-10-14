904ca865c53bee5538be46e37864d46a6575df4d
Rafael Petit <rpetit.dev@gmail.com>
2025-10-14 10:36:28 -0300

    feat: add Eureka client discovery

--- src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -2,8 +2,11 @@ package br.com.alurafood.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PedidosApplication {
 
 	public static void main(String[] args) {
